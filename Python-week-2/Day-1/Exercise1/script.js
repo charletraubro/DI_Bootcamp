ba83db4fc609cc1c9c16a1389b22ac0f77e3b8d3
Charles Trau <charlestraubro@Charless-MacBook-Pro.local>
2022-06-26 16:26:21 +0300

    bootcamp files

---
+++ Python-week-2/Day-1/Exercise1/script.js
@@ -0,0 +1,12 @@
+
+let addressNumber='64';
+let addressStreet= 'Pinkas';
+let country = 'Israel';
+
+let globalAddress = (`${addressStreet} ${addressNumber} ${country}`);
+console.log(globalAddress);
+
+let birth_year='2002'
+let future_year='2025'
+
+console.log(Number(future_year)-Number(birth_year));
---
+++ Python-week-2/Day-1/script.js
@@ -0,0 +1,40 @@
+
+
+// let name='John';
+// let last_name = 'Miller';
+// let space = '';
+// console.log('=>', name+''+ last_name)
+// console.log(`${name} ${last_name}`)
+
+// let a=10;
+// let b='Hello';
+// let c = true;//1
+// let d=false;//0
+// let f=null;
+
+// console.log(typeof(c));
+// var txt = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+// var a = txt.length;
+// console.log(a);
+
+// var str = "Please locate where 'locate' occurs!";
+// var pos = str.indexOf("wh");
+// console.log(pos);
+
+// var str = "Appe, Banana, Kiwi";
+// var res = str.substring(7, 13)
+// console.log(res);
+
+// str = "Pleqse visit Microsoft!";
+// const n =str.replace("Microsoft", "Developers Institute");
+// console.log(n);
+
+// var text2 = text1.toUpperCase();
+// var text2 = text1.toLowerCase();
+
+// var str = "HELLO WORLD";
+// let a = str.charAT(4);
+
+// var str = "HELLO WORLD";
+// str.charCodeAt(0)
+// console.log("charCodeAt", str.charCodeAt(0));
