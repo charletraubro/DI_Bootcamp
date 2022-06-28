f51fad1fcaf81fdc5a95bff7db4cb6c5821d2eaa
charletraubro <107468589+charletraubro@users.noreply.github.com>
2022-06-28 10:31:24 +0300

    Update script.js

--- Python-week-2/Day-2/Daily-Challenge/script.js
+++ Python-week-2/Day-2/Daily-Challenge/script.js
@@ -1,6 +1,6 @@
 
 let sentence ="The movie is not that bad, I like it";
-let sentence1= prompt("Do a sentence with the word not and bad","The movie is not that bad, I like it")
+//let sentence1= prompt("Do a sentence with the word not and bad","The movie is not that bad, I like it")
 let wordNot = sentence.indexOf("not");
 let wordBad = sentence.indexOf("bad");
 let sentence2= sentence1.substring(0 ,wordNot-1);
