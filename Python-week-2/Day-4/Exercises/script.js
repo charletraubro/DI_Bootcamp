d14ae992bd67154176ed3d1863cf95c79231b15b
Charles Trau <charlestraubro@Charless-MacBook-Pro.local>
2022-07-03 09:46:06 +0300

    yes

---
+++ Python-week-2/Day-4/Exercises/script.js
@@ -0,0 +1,75 @@
+// function infoAboutMe(){
+// 	console.log('My names is charles and I like playing football, I am 19 years old');
+// }
+// infoAboutMe()
+
+// function infoAboutPerson(personName,personAge,personFavorite){
+//  console.log(" My name is "+ personName +" my age is "+personAge+" my favorite color is "+personFavorite)
+// }
+// infoAboutPerson("David", 45, "blue")
+// infoAboutPerson("Josh", 12, "yellow")
+//--------------exercise-2----------------------
+
+
+// function calculateTip(){
+// 	let bill = prompt("Put in number")
+// 	if (bill<50) {
+// 		console.log(`Pay ${bill*1.2} dollars`)
+// 	} else if (50<bill<200) {
+// 		console.log(`Pay ${bill*1.15} dollars`)
+// 	} else if (bill>200){
+// 		console.log(`Pay ${bill*1.1} dollars`)
+// 	}
+// }
+// calculateTip()
+
+// let sum= 0
+// function isDivisible(){
+// 	for(let i=0; i<=500; i++){
+// 	if (i%23==0) {
+// 	console.log(i);
+// 	sum += i;
+// }
+// }
+
+// }
+// isDivisible()
+// console.log(sum);
+
+// let stock = { 
+//     "banana": 6, 
+//     "apple": 0,
+//     "pear": 12,
+//     "orange": 32,
+//     "blueberry":1
+// }  
+
+// let prices = {    
+//     "banana": 4, 
+//     "apple": 2, 
+//     "pear": 1,
+//     "orange": 1.5,
+//     "blueberry":10
+// } 
+
+// let shoppingList= ["banana","orange","apple"];
+
+// function myBill() {
+//     bill = 0
+//     for (let i of shoppingList) {
+//         if (i in stock && stock[i] !==0) {
+//             bill +=prices[i];
+//             stock[i] -= 1;
+//         } 
+        
+//     } 
+//     console.log(bill);
+// }
+
+// myBill()
+
+function hotelCost() {
+	let nights = prompt("How many nights would you like to stay?")
+	
+}
+
---
+++ Python-week-2/Day-4/lesson/script.js
@@ -0,0 +1,104 @@
+// function checkEmail(){
+// 	let email= 'charles.trau11@gmail.com'
+// 	if (true) {
+// 		return email
+// 	}
+// 	else{
+// 		return false
+// 	}
+// }
+// let a = checkEmail()
+// console.log('a=>',a);
+
+// let b = checkEmail()
+// console.log('b=>',b);
+//----------------------------------
+
+// function checkEmail(parameters){
+// 	return parameters
+// }
+// let email = 'charles.trau11@gmail.com'
+// let a = checkEmail(email)
+// console.log('a=>', a);
+
+// let b = checkEmail('tal@gmail.com')
+// console.log(b);
+
+//------------------------------------
+
+// function checkEmail(x, y){
+// 	return x + y
+// }
+
+// let a = checkEmail(5,6)
+// console.log('a=>',a);
+
+// let b = checkEmail(1,2)
+// console.log(b);
+
+//-------------------------------
+
+// function checkEmail(x,y){
+// 	let arr = [];
+// 	arr.push(x);
+// 	arr.push(y);
+// 	arr.push(x+y);
+// 	return arr
+// }
+
+// let a = checkEmail(5,6)
+// console.log('a=>',a);
+
+// let b = checkEmail(1,2)
+// console.log(b);
+
+// function add(x,y,z){//10,6,3
+// 	return minus(x+y, z)//16,3
+// }
+// functionminus(x,y){//16,3
+// 	return x-y//16-3
+// }
+// let ret = add(10,6,3);
+
+// console.log(ret)//13
+
+// function add(x,y,z=2){
+// 	return minus(x+y, z)
+// }
+// functionminus(x,y){
+// 	return x-y
+// }
+// let ret = add(10,6,3);
+
+// console.log(ret)
+
+// let ret = add(20,10);
+// console.log(ret);
+
+// function add (x,y){
+// 	return x+y
+// }
+
+
+// const x = function(){
+// 	console.log('my x function')
+// };
+// let greeting= x();
+
+// const z = (val) => {
+// return val + 'with z function'
+// }
+// let h =z(greeting);
+// console.log(h);
+
+// function familyAge(myAge) {
+// 	let mumAge = myAge*2
+// 	let dadAge = mumAge*1.2;
+// 	return (myAge + "is my age," + mumAge+ "is my mum's age," + dadAge+ "is my dad's age.")
+// }
+// console.log(familyAge(19));
+
+
+
+
+
